2dbad167585f249625d52e3642554902bd573f82
OssieFromDK <50819244+OssieFromDK@users.noreply.github.com>
2025-02-04 15:31:55 +0100

    Assets + Base Scene

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject GreenLight;
+    [SerializeField] private GameObject YellowLight; 
+    [SerializeField] private GameObject RedLight; 
+    
+    
+    void Start()
+    {
+        YellowLight.SetActive(false);   
+        RedLight.SetActive(false);
+        GreenLight.SetActive(true);
+    }
+
+    public void SetYellowLight()
+    {
+        GreenLight.SetActive(false);
+        YellowLight.SetActive(true);
+        RedLight.SetActive(false);
+    }
+    
+    public void SetRedLight()
+    {
+        GreenLight.SetActive(false);
+        YellowLight.SetActive(false);
+        RedLight.SetActive(true);
+    }
+    
+    public void SetGreenLight()
+    {
+        GreenLight.SetActive(true);
+        YellowLight.SetActive(false);
+        RedLight.SetActive(false);
+    }
+}
